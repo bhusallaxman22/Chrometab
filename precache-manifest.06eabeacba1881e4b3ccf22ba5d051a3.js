cd5e573438146638bb1f9523f8be0352732ae545
Laxman Bhushal <laxmanbhusal612@gmail.com>
2019-09-19 17:54:10 +0545

    hmm

--- precache-manifest.06eabeacba1881e4b3ccf22ba5d051a3.js
+++ precache-manifest.06eabeacba1881e4b3ccf22ba5d051a3.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ee9dc8445dd3df180a4852f98f9d6135",
-    "url": "github.com/Chrometab/index.html"
+    "url": "https://bhusallaxman22.github.io/Chrometab/index.html"
   },
   {
     "revision": "dc15e20bf3cf41a7f461",
-    "url": "github.com/Chrometab/static/css/main.9a0630bb.chunk.css"
+    "url": "https://bhusallaxman22.github.io/static/css/main.9a0630bb.chunk.css"
   },
   {
     "revision": "a89d59427be2e9685dfa",
-    "url": "github.com/Chrometab/static/js/2.7f514c60.chunk.js"
+    "url": "https://bhusallaxman22.github.io/static/js/2.7f514c60.chunk.js"
   },
   {
     "revision": "dc15e20bf3cf41a7f461",
-    "url": "github.com/Chrometab/static/js/main.c44f2e6f.chunk.js"
+    "url": "https://bhusallaxman22.github.io/static/js/main.c44f2e6f.chunk.js"
   },
   {
     "revision": "f06b7c863203c821eb66",
-    "url": "github.com/Chrometab/static/js/runtime~main.68f6b27c.js"
+    "url": "https://bhusallaxman22.github.io/Chrometab/static/js/runtime~main.68f6b27c.js"
   }
 ]);
