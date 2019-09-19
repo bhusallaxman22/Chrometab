c0bbc1c01ef4a35445d07dc381c538ba34aaf123
Laxman Bhushal <laxmanbhusal612@gmail.com>
2019-09-19 17:33:51 +0545

    ummm

---
+++ precache-manifest.06eabeacba1881e4b3ccf22ba5d051a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9dc8445dd3df180a4852f98f9d6135",
+    "url": "github.com/Chrometab/index.html"
+  },
+  {
+    "revision": "dc15e20bf3cf41a7f461",
+    "url": "github.com/Chrometab/static/css/main.9a0630bb.chunk.css"
+  },
+  {
+    "revision": "a89d59427be2e9685dfa",
+    "url": "github.com/Chrometab/static/js/2.7f514c60.chunk.js"
+  },
+  {
+    "revision": "dc15e20bf3cf41a7f461",
+    "url": "github.com/Chrometab/static/js/main.c44f2e6f.chunk.js"
+  },
+  {
+    "revision": "f06b7c863203c821eb66",
+    "url": "github.com/Chrometab/static/js/runtime~main.68f6b27c.js"
+  }
+]);
