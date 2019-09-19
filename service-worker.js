f5dc6adc5769bf4efe0efcdc3ab86820dabfcd4e
Laxman Bhusal <laxmanbhusal612@gmail.com>
2019-09-19 19:47:25 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "github.com/Chrometab/precache-manifest.06eabeacba1881e4b3ccf22ba5d051a3.js"
+  "bhusallaxman22.github.io/Chrometab/precache-manifest.1ed05572a05136711cf5b8e1bf6ef2d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("github.com/Chrometab/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("bhusallaxman22.github.io/Chrometab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
